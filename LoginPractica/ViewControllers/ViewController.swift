c4d90d4fe4883ffb5967b958568e366f2212f377
Mauricio Juárez <mau@MacBook-Air-de-Mauricio-4.local>
2022-09-26 20:31:52 -0500

    1ra versión

---
+++ LoginPractica/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginPractica
+//
+//  Created by Mauricio Juárez on 26/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
